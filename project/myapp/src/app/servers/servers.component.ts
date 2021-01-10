80264653dff6fb9715f2a92de58515a23d077c61
ishanRox <roxishan8@gmail.com>
2021-01-10 22:32:32 +0530

    added new toggle
    changes from 5

--- project/myapp/src/app/servers/servers.component.ts
+++ project/myapp/src/app/servers/servers.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  //styleUrls: ['./servers.component.css']
+  styleUrls: ['./servers.component.css']
 
 })
 export class ServersComponent implements OnInit {
@@ -11,20 +11,33 @@ export class ServersComponent implements OnInit {
   serverName: string = "not available Now";
   currentServerName = 'TestServer';
   serverCreated = false;
+  servers = ['TestServer1', 'TestServer2'];
+  //assignment
+  toggleFlag: boolean = false;
+  counterArray = [];
 
   constructor() {
+
   }
 
   ngOnInit(): void {
   }
 
   onAddServer() {
-
+    this.servers.push(this.serverName);
     this.serverCreated = true;
     this.serverName = "Server started and running !";
 
   }
   onUpdateServerName(event: Event) {
     this.currentServerName = (<HTMLInputElement>event.target).value;
+  }
+
+
+  onToggleFlag() {
+    console.log("toggled");
+    this.counterArray.push(new Date());
+    this.toggleFlag = !this.toggleFlag;
+
   }
 }
