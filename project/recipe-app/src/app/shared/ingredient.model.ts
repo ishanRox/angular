e84ed3acdf3a47c22a9c9e96e982adbb92e66378
ishanRox <roxishan8@gmail.com>
2021-02-11 20:34:11 +0530

    added recipe list components

---
+++ project/recipe-app/src/app/shared/ingredient.model.ts
@@ -0,0 +1,13 @@
+export class Ingredient{
+    // public name:string;public amount:number;
+
+    // constructor(name:string,amount:number) {
+        
+    // }
+
+    //above code done by shorthand
+
+    constructor(public name:string,public amount:number) {
+        
+    }
+}
