fb0d2db206a69def4339536097209e7eb5ae1edc
ishanRox <roxishan8@gmail.com>
2021-02-21 22:26:43 +0530

    recipe book add ingredients completed

--- project/recipe-app/src/app/app.component.ts
+++ project/recipe-app/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'recipe-app';
-selectedItemName:string='';
+selectedItemName:string='Recipes';
 
 onSelected(selectedItem: string){
 console.log(selectedItem);
