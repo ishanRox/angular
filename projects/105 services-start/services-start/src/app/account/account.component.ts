c7ae6569df7d4c91409bc27b26a13df25966b20a
ishanRox <roxishan8@gmail.com>
2021-02-28 12:58:36 +0530

    parent to child service(Hierarchical injector)  created

--- projects/105 services-start/services-start/src/app/account/account.component.ts
+++ projects/105 services-start/services-start/src/app/account/account.component.ts
@@ -1,18 +1,26 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { AccountService } from '../account.service';
+import { LogggingService } from '../login.service';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
-  styleUrls: ['./account.component.css']
+  styleUrls: ['./account.component.css'],
+  providers: [LogggingService]
 })
 export class AccountComponent {
-  @Input() account: {name: string, status: string};
+  //we still need input because we still reciving data from outside
+  @Input() account: { name: string, status: string };
   @Input() id: number;
-  @Output() statusChanged = new EventEmitter<{id: number, newStatus: string}>();
+  // @Output() statusChanged = new EventEmitter<{ id: number, newStatus: string }>();
 
+  constructor(private loggingService: LogggingService, private accountService: AccountService) {
 
+  }
   onSetTo(status: string) {
-    this.statusChanged.emit({id: this.id, newStatus: status});
-    console.log('A server status changed, new status: ' + status);
+    // this.statusChanged.emit({ id: this.id, newStatus: status });
+    this.accountService.updateStatus(this.id, status);
+    this.loggingService.logStatusChange(status);
+    // console.log('A server status changed, new status: ' + status);
   }
 }
--- projects/105 services-start/services-start/src/app/new-account/new-account.component.ts
+++ projects/105 services-start/services-start/src/app/new-account/new-account.component.ts
@@ -1,24 +1,31 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { AccountService } from '../account.service';
 import { LogggingService } from '../login.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+  styleUrls: ['./new-account.component.css'],
+  providers: [LogggingService]
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{ name: string, status: string }>();
+  // @Output() accountAdded = new EventEmitter<{ name: string, status: string }>();
 
+
+  constructor(private loggingService: LogggingService,
+    private accountService: AccountService) {
+
+  }
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
+    // this.accountAdded.emit({
+    //   name: accountName,
+    //   status: accountStatus
+    // });
+    //other than emiting and making other to detect it 
+    //we our self add data toa service
 
-    //stupid way or using a service
-    const service = new LogggingService();
-    service.logStatusChange(accountStatus);
-
+    this.accountService.addAccount(accountName, accountStatus);
+    this.loggingService.logStatusChange(accountStatus);
     // console.log('A server status changed, new status: ' + accountStatus);
 
   }
