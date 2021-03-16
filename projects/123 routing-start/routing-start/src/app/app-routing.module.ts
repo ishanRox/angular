7f0549228da9a4d44b8700653967bf11c8dd1001
ishanRox <roxishan8@gmail.com>
2021-03-16 23:41:03 +0530

    app-routing outsourced

---
+++ projects/123 routing-start/routing-start/src/app/app-routing.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { EditServerComponent } from "./servers/edit-server/edit-server.component";
+import { ServerComponent } from "./servers/server/server.component";
+import { ServersComponent } from "./servers/servers.component";
+import { UserComponent } from "./users/user/user.component";
+import { UsersComponent } from "./users/users.component";
+
+
+const appRoutes: Routes = [
+    { path: '', component: HomeComponent },
+    {
+        path: 'users', component: UsersComponent, children: [
+            { path: ":id/:name", component: UserComponent },
+        ]
+    },
+    {
+        path: "servers", component: ServersComponent, children: [
+            { path: ":id", component: ServerComponent },
+            { path: ":id/edit", component: EditServerComponent },
+        ]
+    },
+    { path: 'not-found', component: PageNotFoundComponent },
+    //wildcard to catch all undefined routes and redirect them to not-found
+    //please remember that this need to be the final one becase
+    // routes in this array parse from top to bottom
+    { path: '**', redirectTo: '/not-found' },
+];
+
+//we add routers to appRoutes array and add that to RouterModule
+//And Export that array to it
+@NgModule({
+    imports: [
+        RouterModule.forRoot(appRoutes)
+    ],
+    exports:[
+        RouterModule
+    ]
+})
+export class AppRoutingModule {
+
+}
--- projects/123 routing-start/routing-start/src/app/app.module.ts
+++ projects/123 routing-start/routing-start/src/app/app.module.ts
@@ -13,26 +13,7 @@ import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import { RouterModule, Routes } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-
-const appRoutes: Routes = [
-  { path: '', component: HomeComponent },
-  {
-    path: 'users', component: UsersComponent, children: [
-      { path: ":id/:name", component: UserComponent },
-    ]
-  },
-  {
-    path: "servers", component: ServersComponent, children: [
-      { path: ":id", component: ServerComponent },
-      { path: ":id/edit", component: EditServerComponent },
-    ]
-  },
-  { path: 'not-found', component: PageNotFoundComponent },
-  //wildcard to catch all undefined routes and redirect them to not-found
-  //please remember that this need to be the final one becase
-  // routes in this array parse from top to bottom
-  { path: '**', redirectTo: '/not-found' },
-];
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -48,7 +29,8 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes)
+    // RouterModule.forRoot(appRoutes)
+    AppRoutingModule
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
